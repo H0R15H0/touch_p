37d5fe2d5a65776c9f504597be98e8fc2808281e
H0R15H0 <shohori13@gmail.com>
2021-06-29 00:11:02 +0900

    Update: Initial code!

--- lib/touch_p.rb
+++ lib/touch_p.rb
@@ -1,6 +1,39 @@
 require "touch_p/version"
 
 module TouchP
-  class Error < StandardError; end
-  # Your code goes here...
+  def self.exe
+    return if !is_valid?(ARGV)
+    touch_p(ARGV)
+  end
+
+  def self.touch_p(argv)
+    argv.each do |file_path|
+      if file_path.include?("/")
+        dir = extract_dir(file_path)
+        begin
+          FileUtils.mkdir_p(dir)
+        rescue Errno::EEXIST
+        end
+      end
+    end
+
+    begin
+      FileUtils.touch(argv)
+    rescue => exception
+      puts exception
+    end
+  end
+  
+  private
+  def self.is_valid?(argv)
+    return puts("Usage: touch_p FILE...") if argv[0] == "--help"
+    return puts("touch_p: '#{TouchP::VERSION}'") if argv[0] == "--version"
+    return puts("Try 'touch_p --help' for more information.") if argv.length == 0 || argv[0][0] == "-"
+    return true
+  end
+
+  def self.extract_dir(file_path)
+    return file_path if file_path[-1] == "/"
+    return file_path.split("/").slice(0..-2).join("/")
+  end
 end
